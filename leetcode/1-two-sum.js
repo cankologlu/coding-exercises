1b0ee409eb61e5716fa7f56214096c7050269b1f
Can Kologlu <mckologlu@gmail.com>
2023-04-24 15:32:44 -0400

    nested loop exercise

---
+++ leetcode/1-two-sum.js
@@ -0,0 +1,15 @@
+var twoSum = function(nums, target) {
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i === j) {
+                j++
+            }
+            if ((nums[i] + nums[j]) === target) {
+                result.push(i)
+                result.push(j)
+                return result;
+            }
+        }
+    }
+};
