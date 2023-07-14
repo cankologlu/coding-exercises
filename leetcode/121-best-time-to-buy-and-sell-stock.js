68ef914ba28e222af3f7fef236c9f08e557c57fd
Can Kologlu <mckologlu@gmail.com>
2023-07-13 21:35:16 -0400

    solution2

---
+++ leetcode/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+const maxProfit = (prices) => {
+  let minPrice = prices[0];
+  let max = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    let profit = prices[i] - minPrice;
+
+    if (profit > max) {
+      max = profit;
+    }
+
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    }
+  }
+
+  return max;
+};
