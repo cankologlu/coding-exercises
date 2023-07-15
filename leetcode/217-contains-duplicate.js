cf86cec22d047f8d56bd5c90cfee9d8911ee9799
Can Kologlu <mckologlu@gmail.com>
2023-07-15 14:21:07 -0400

    lol

--- leetcode/217-contains-duplicate.js
+++ leetcode/217-contains-duplicate.js
@@ -1,3 +1,9 @@
-const containsDuplicate = () => {
+const containsDuplicate = (nums) => {
   
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.indexOf(nums[i], i + 1) !== -1) {
+      return true;
+    }
+  }
+  return false;
 }
